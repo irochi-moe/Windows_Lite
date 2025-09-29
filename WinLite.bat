b1984eb0b94ce6feabf0df0c6e792ec502c35080
Irochi <me@irochi.moe>
2025-09-30 01:24:30 +0900

    Bump ver

--- WinLite.bat
+++ WinLite.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Windows Lite Self Edition by Irochi (https://irochi.moe)
-rem v2.2
+rem v2.3
 
 cd /d %~dp0
 whoami /groups | findstr /i "S-1-16-12288" >nul
@@ -41,7 +41,7 @@ echo.
 echo.
 echo  by Irochi (https://irochi.moe)
 echo.
-echo               v2.2
+echo               v2.3
 echo.
 echo 式式式式式式式式式式式式式式式式式式式式式式式式式式式式式式
 echo.
